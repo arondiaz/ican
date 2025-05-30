fcb58b6e7f399e4ee590ee501fce080d347dd2e9
Arón Díaz <arondiaz.cpn@gmail.com>
2025-05-30 17:02:36 -0300

    first commit

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    extend: {
+      fontFamily: {
+        sans: "var(--font-archivo)",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md: "2rem",
+          lg: "4rem",
+        },
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "red-orange": "var(--color-red-orange)",
+        // "red-orange": {
+        //   500: 'color-mix(in srgb, theme("colors.red.500")50%,theme("colors.orange.500") 50%)',
+
+        // },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
