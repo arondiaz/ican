f745c99a774d644d54a7ab35f0dcca3d1a424a6f
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-02 15:43:08 -0300

    updated layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const archivo = Archivo({
   display: "swap",
@@ -35,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         //bg color
-        className={`${archivo.variable}  ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${archivo.variable} antialiased bg-teal-50`}
       >
         {children}
       </body>
