ecd044147f547d6ed5532f4afb8fe38c3588cfc2
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-04 15:26:17 -0300

    implemented cors on backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import workerRoutes from "./routes/workers.routes.js";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "*", // o '*' para permitir todos (menos seguro)
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type",
+};
 
 const app = express();
 
 //middleware
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use(workerRoutes);
 
