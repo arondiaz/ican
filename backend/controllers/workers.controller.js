75046a55d74fc9e2f809a47af2393f90e613d8ab
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-04 14:51:07 -0300

    implemented controllers to update and delete workers and update the others

--- backend/controllers/workers.controller.js
+++ backend/controllers/workers.controller.js
@@ -1,8 +1,14 @@
+import { Category } from "../models/Categories.js";
 import { Worker } from "../models/Worker.js";
 
 export const getWorkers = async (req, res) => {
   try {
-    const workers = await Worker.findAll();
+    const workers = await Worker.findAll({
+      include: {
+        model: Category,
+        through: { attributes: [] },
+      },
+    });
     res.json(workers);
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -11,7 +17,7 @@ export const getWorkers = async (req, res) => {
 
 export const createWorker = async (req, res) => {
   try {
-    const { name, description, phone, city } = req.body;
+    const { name, description, phone, city, categoryIds } = req.body;
 
     const newWorker = await Worker.create({
       name,
@@ -19,7 +25,56 @@ export const createWorker = async (req, res) => {
       phone,
       city,
     });
-    res.json(newWorker);
+
+    if (categoryIds && categoryIds.length > 0) {
+      await newWorker.setCategories(categoryIds); // setCategories acepta array de IDs
+    }
+    const workerWithCategories = await Worker.findByPk(Worker.id, {
+      include: Category,
+    });
+    res.status(200).json(workerWithCategories);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateWorker = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, city, phone, categoryIds } = req.body;
+
+    const updateWorker = await Worker.findByPk(id, {
+      include: {
+        model: Category,
+        through: { attributes: [] },
+      },
+    });
+    if (categoryIds && categoryIds.length > 0) {
+      await updateWorker.setCategories(categoryIds);
+    }
+    updateWorker.name = name;
+    updateWorker.description = description;
+    updateWorker.city = city;
+    updateWorker.phone = phone;
+    updateWorker.categoryIds = categoryIds;
+    await updateWorker.save();
+
+    res.json(updateWorker);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteWorker = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const worker = await Worker.destroy({ where: { id } });
+    if (worker === 0) {
+      return res
+        .status(404)
+        .json({ message: "worker with this id doesn´t exist" });
+    }
+    res.status(204).send();
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- backend/routes/workers.routes.js
+++ backend/routes/workers.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getWorkers, createWorker } from "../controllers/workers.controller.js";
+import { getWorkers, createWorker, updateWorker, deleteWorker } from "../controllers/workers.controller.js";
 
 const router = Router();
 
 router.get("/workers", getWorkers);
 router.post("/workers", createWorker);
-// router.put("/workers/:id");
-// router.delete("/workers/:id");
+router.put("/workers/:id", updateWorker);
+router.delete("/workers/:id", deleteWorker);
 // router.get("/workers/:id");
 
 export default router;
