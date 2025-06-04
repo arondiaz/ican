2cae115b41471c28421dbaa08db7db5c53d828df
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-04 14:49:48 -0300

    created categories for develop

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 import "./models/index.js";
-
+import { Category } from "./models/Categories.js";
 
 const port = 4444;
 
 async function main() {
   try {
-    await sequelize.sync({force: false});
+    await sequelize.sync({ force: false });
+    const count = await Category.count();
+    if (count === 0) {
+      await Category.bulkCreate([
+        { name: "Electricista" },
+        { name: "Plomero" },
+        { name: "Carpintero" },
+      ]);
+      console.log("Categorías iniciales creadas");
+    }
     app.listen(port);
 
     console.log("Server working on port", port);
