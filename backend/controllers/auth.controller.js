bf08677e5401563b3dbe7268956009f8c081ac97
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-14 14:54:24 -0300

    implemented cookie on auth controller and middleware

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,25 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { User } from "../models/User.js"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { User } from "../models/User.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ where: { email } });
 
-  if (!user || !await bcrypt.compare(password, user.password)) {
-    return res.status(401).json({ message: 'Credenciales inválidas' });
+  if (!user || !(await bcrypt.compare(password, user.password.trim()))) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
   }
 
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-  res.json({ token });
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  // se guarda el token en las cookies
+  res.cookie("sessiontokenadms", token, {
+      httpOnly: true, // No accesible desde JS en el navegador
+      secure: process.env.NODE_ENV === "production", // solo HTTPS en producción
+      sameSite: "lax", // protección CSRF básica
+      maxAge: 1000 * 60 * 60 * 24, // 1 día
+    })
+    .json({ success: true });
 };
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,8 @@
 import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-
-  if (!token) return res.status(403).json({ message: 'Token faltante' });
+const token = req.cookies.token;
+  if (!token) return res.status(401).json({ error: "No autorizado" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
