7e3093ad0c5730b2bfac9447a0475c6b3ad23015
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-14 14:52:27 -0300

    added cookie parser

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ import workerRoutes from "./routes/workers.routes.js";
 import categoryRoutes from "./routes/categories.route.js";
 import authRoutes from "./routes/auth.route.js"
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const corsOptions = {
-  origin: "*", // o '*' para permitir todos (menos seguro)
+  origin: "http://localhost:3000", // o '*' para permitir todos (menos seguro)
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type",
+   credentials: true, 
 };
 
 const app = express();
@@ -16,6 +18,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.use(workerRoutes);
 app.use(categoryRoutes);
