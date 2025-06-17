603a77adc810a83a324bd88c8973dbe084791a7f
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-17 11:36:16 -0300

    added metrics controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { User } from "../models/User.js";
+import { Worker } from "../models/Worker.js";
+import { Category } from "../models/Categories.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
@@ -15,11 +17,63 @@ export const login = async (req, res) => {
   });
 
   // se guarda el token en las cookies
-  res.cookie("sessiontokenadms", token, {
+  res
+    .cookie("sessiontokenadms", token, {
       httpOnly: true, // No accesible desde JS en el navegador
       secure: process.env.NODE_ENV === "production", // solo HTTPS en producción
       sameSite: "lax", // protección CSRF básica
       maxAge: 1000 * 60 * 60 * 24, // 1 día
     })
     .json({ success: true });
+};
+
+export const metrics = async (req, res) => {
+  try {
+    const workers = await Worker.findAll({
+      include: {
+        model: Category,
+        through: { attributes: [] },
+      },
+    });
+    const metrics = {
+      total: workers.length,
+      porCategoria: {
+        Electricistas: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Electricista")
+        ).length,
+        Gasistas: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Gasista")
+        ).length,
+        Plomeros: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Plomero")
+        ).length,
+        Carpinteros: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Carpintero")
+        ).length,
+        Cerrajeros: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Cerrajero")
+        ).length,
+        Mecanicos: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Mecanico")
+        ).length,
+        Gasistas: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Mecanico")
+        ).length,
+        Gomeros: workers.filter((w) =>
+          w.categories.some((c) => c.name === "Gomero")
+        ).length,
+        "Instalador de aires": workers.filter((w) =>
+          w.categories.some((c) => c.name === "Instalador de aire")
+        ).length,
+      },
+      porCiudad: {
+        Rosario: workers.filter((w) => w.city === "Rosario").length,
+        Casilda: workers.filter((w) => w.city === "Casilda").length,
+        "Santa Fe": workers.filter((w) => w.city === "Santa Fe").length,
+      },
+    };
+    res.json(metrics);
+  } catch (err) {
+    res.status(500).json({ error: "Error al calcular métricas" });
+  }
 };
