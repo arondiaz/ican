fa95f49b4abe60b8ae89e568eb2af6a133928c9b
Arón Díaz <arondiaz.cpn@gmail.com>
2025-06-20 14:06:56 -0300

    added domain on next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {  images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', "res-console.cloudinary.com"],
   },};
 
 export default nextConfig;
