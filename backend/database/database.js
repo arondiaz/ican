37dadd1a7676144ba43b78a91ce4a06d8b965dec
Arón <arondiaz.cpn@gmail.com>
2025-06-26 14:23:57 -0300

    connected db to railway

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
-);
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//   }
+// );
+
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
