65e8372c9c1de657306d1d67727fd9353e867c8f
Arón <arondiaz.cpn@gmail.com>
2025-06-27 22:38:02 -0300

    refactor result page

--- frontend/src/app/results/page.tsx
+++ frontend/src/app/results/page.tsx
@@ -1,13 +1,49 @@
-// /* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import FilteredCards from "../components/FilteredCards";
+import { IWorker } from "../utils/interface";
+import Navbar from "../components/Navbar";
 
-// import { Suspense } from "react";
-// import ResultsContent from "../components/ResultsContent";
+const Page = () => {
+  const [resultSearch, setResultSearch] = useState([] as IWorker[]);
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
 
-// const Page = () => {
-//   return (
-//     <Suspense fallback={<div>Cargando...</div>}>
-//       <ResultsContent />;
-//     </Suspense>
-//   );
-// };
-// export default Page;
+  useEffect(() => {
+    const URL = process.env.NEXT_PUBLIC_API;
+
+    const getSearch = async () => {
+      const url = `${URL}/categories/${query}`;
+
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setResultSearch(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getSearch();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6 mb-10">
+        {resultSearch.length > 0 ? (
+          resultSearch.map((worker) => (
+            <FilteredCards key={worker.id} worker={worker} />
+          ))
+        ) : (
+          <div className="flex justify-center items-center">
+            <h2>No hay resultados</h2>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Page;
